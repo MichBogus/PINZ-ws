f24fd5b72c6bbaa7deac4f4bdf7aa23c3648a544
Michal Bogus <m@m.com>
2017-10-23 22:19:22 +0200

    Added item controller

--- src/main/kotlin/controller/item/UserItemsController.kt
+++ src/main/kotlin/controller/item/UserItemsController.kt
@@ -1,9 +1,31 @@
 package controller.item
 
+import controller.base.WSResponseEntity
+import model.workflow.request.LoginUserRequest
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(value = "/item")
 class UserItemsController : UserItemsControllerMappings {
+
+    override fun addItem(request: LoginUserRequest): WSResponseEntity {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun deleteItem(request: LoginUserRequest): WSResponseEntity {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCompanyItems(request: LoginUserRequest): WSResponseEntity {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemByToken(request: LoginUserRequest): WSResponseEntity {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemForUser(request: LoginUserRequest): WSResponseEntity {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
--- src/main/kotlin/controller/item/UserItemsControllerMappings.kt
+++ src/main/kotlin/controller/item/UserItemsControllerMappings.kt
@@ -1,4 +1,41 @@
 package controller.item
 
+import controller.base.WSResponseEntity
+import model.workflow.request.LoginUserRequest
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import javax.validation.Valid
+
 interface UserItemsControllerMappings {
+
+    @RequestMapping(value = "/addItem",
+            method = arrayOf(RequestMethod.POST),
+            consumes = arrayOf("application/json"),
+            produces = arrayOf("application/json"))
+    fun addItem(@Valid @RequestBody request: LoginUserRequest): WSResponseEntity
+
+    @RequestMapping(value = "/deleteItem",
+            method = arrayOf(RequestMethod.POST),
+            consumes = arrayOf("application/json"),
+            produces = arrayOf("application/json"))
+    fun deleteItem(@Valid @RequestBody request: LoginUserRequest): WSResponseEntity
+
+    @RequestMapping(value = "/getAllCompanyItems",
+            method = arrayOf(RequestMethod.POST),
+            consumes = arrayOf("application/json"),
+            produces = arrayOf("application/json"))
+    fun getCompanyItems(@Valid @RequestBody request: LoginUserRequest): WSResponseEntity
+
+    @RequestMapping(value = "/getItemByToken",
+            method = arrayOf(RequestMethod.POST),
+            consumes = arrayOf("application/json"),
+            produces = arrayOf("application/json"))
+    fun getItemByToken(@Valid @RequestBody request: LoginUserRequest): WSResponseEntity
+
+    @RequestMapping(value = "/getItemForUser",
+            method = arrayOf(RequestMethod.POST),
+            consumes = arrayOf("application/json"),
+            produces = arrayOf("application/json"))
+    fun getItemForUser(@Valid @RequestBody request: LoginUserRequest): WSResponseEntity
 }
